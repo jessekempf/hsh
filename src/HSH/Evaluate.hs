e232dc171a05edbd514118c4fad3b8cc4c625a6e
Jesse Kempf <jessekempf@gmail.com>
2016-05-23 00:44:45 -0700

    add rudimentary support for running subprocesses.

--- src/HSH/Evaluate.hs
+++ src/HSH/Evaluate.hs
@@ -4,9 +4,15 @@ import HSH.CommandLineParse
 
 import Control.Monad.State
 import System.IO
+import System.Process
+
+import qualified System.Environment as SysEnv
 
 import qualified Data.Map as Map
 import Data.Maybe
+import Data.Foldable
+
+import GHC.IO.Exception (ExitCode(..))
 
 data ShellState = ShellState { envVars :: Map.Map String String } deriving (Eq, Show)
 
@@ -32,6 +38,20 @@ evaluate (GetEnv varname) = do
 
 evaluate DebugState = get >>= lift . print
 
+evaluate (ExternalCommand command args) = do
+  currentState <- get
+
+  (_i, _o, _e, ph) <- lift $ createProcess (proc command args) { env = Just $ Map.toList $ envVars currentState }
+
+  exitCode <- lift $ waitForProcess ph
+
+  let numericExitCode = case exitCode of
+                          ExitSuccess -> 0
+                          ExitFailure num -> num
+
+  let newState = setEnv "?" (show numericExitCode) currentState
+
+  put newState
 
 {- REPL Functions -}
 rep :: StateT ShellState IO ()
@@ -40,7 +60,13 @@ rep = parseLine <$> lift getLine >>= evaluate
 repl :: StateT ShellState IO ()
 repl = forever rep
 
+clearEnv :: IO ()
+clearEnv = do
+  vars <- map fst <$> SysEnv.getEnvironment
+  traverse_ SysEnv.unsetEnv vars
+
 runREPL :: IO ()
 runREPL = do
   hSetBuffering stdout NoBuffering
+  clearEnv
   evalStateT repl defaultShellState
