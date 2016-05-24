4ddbd0d2f5b1d2a44f82ff43f2291628ce2ae5f2
Jesse Kempf <jessekempf@gmail.com>
2016-05-24 15:37:35 -0700

    docs.

--- README.md
+++ README.md
@@ -11,15 +11,21 @@ Well, a few reasons:
 ## Overview
 
 ### Features
-Currently: None
+* Single-command parser
+* Support for env vars through `setenv` and `getenv`
+
+### Misfeatures
+* Attempting to run a command that doesn't exist crashes the shell
+* Mis-invoking builtins (like `setenv foo` when the correct invocation is `setenv foo bar`) causes the shell to treat `setenv` as an external command.
+
 
 ### Non-Features
 This is a programming exercise focused on an interactive command interpreter. I haven't much interest right now in building support for Bourne-style scriptability.
 
-### To-Do
+### To-Do List
 1. Basic command-line interpreter.
-	1. Shell-ish parser
-	1. `ENV` var support
+	1. (DONE) Shell-ish parser
+	1. (DONE) `ENV` var support
 	1. Trivial prompt generator
 	1. Fundamental builtins
 1. Command Look-Up Table creation from `$PATH`
--- src/HSH/Evaluate.hs
+++ src/HSH/Evaluate.hs
@@ -14,30 +14,45 @@ import Data.Foldable
 
 import GHC.IO.Exception (ExitCode(..))
 
-data ShellState = ShellState { envVars :: Map.Map String String } deriving (Eq, Show)
+data ShellState = ShellState { envVars :: Map.Map EnvVarName EnvVarValue } deriving (Eq, Show)
 
-setEnv :: String -> String -> ShellState -> ShellState
+type EnvVarName = String
+type EnvVarValue = String
+
+{-
+-- ENV var manipulation
+-}
+
+-- | Set an environment variable. Takes a name, value, and existing state and returns a
+-- modified state.
+setEnv :: EnvVarName -> EnvVarValue -> ShellState -> ShellState
+
 setEnv name val shellstate =
   shellstate { envVars = Map.insert name val (envVars shellstate) }
 
+-- | The default shell state.
 defaultShellState :: ShellState
 defaultShellState = ShellState { envVars = Map.empty }
 
-{- The Evaluator -}
+-- | Evaluate a shell command abstract syntax tree.
 evaluate :: ShellAST -> StateT ShellState IO ()
 
+{- setenv -}
 evaluate (SetEnv varname value) = do
   newState <- setEnv varname value <$> get
   put newState
 
+{- getenv -}
 evaluate (GetEnv varname) = do
   env <- envVars <$> get
   lift $ putStrLn $ fromMaybe
                     ("Undefined environment variable '" ++ varname ++ "'.")
                     (Map.lookup varname env)
 
+{- showstate -}
 evaluate DebugState = get >>= lift . print
 
+{- Invoke external commands -}
 evaluate (ExternalCommand command args) = do
   currentState <- get
 
@@ -54,17 +69,24 @@ evaluate (ExternalCommand command args) = do
   put newState
 
 {- REPL Functions -}
+
+-- | Read, evaluate, and print a single command.
 rep :: StateT ShellState IO ()
 rep = parseLine <$> lift getLine >>= evaluate
 
+-- | Add looping to the REP.
 repl :: StateT ShellState IO ()
 repl = forever rep
 
+-- | The 'clearEnv' function clears out the running process's ENV vars. This must be run
+-- at shell start, otherwise 'System.Process.createProcess' will behave as if there is an
+-- implicit merge between ShellState's envVars and the process's inherited `ENV`.
 clearEnv :: IO ()
 clearEnv = do
   vars <- map fst <$> SysEnv.getEnvironment
   traverse_ SysEnv.unsetEnv vars
 
+-- | Run the Shell REPL.
 runREPL :: IO ()
 runREPL = do
   hSetBuffering stdout NoBuffering
