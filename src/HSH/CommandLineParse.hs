a197240889ee084a5da92d98132d4339f9f761bc
Jesse Kempf <jessekempf@gmail.com>
2016-05-29 19:25:17 -0700

    Vastly simplify the findNextVariableReference FSM.

--- src/HSH/CommandLineParse.hs
+++ src/HSH/CommandLineParse.hs
@@ -1,13 +1,9 @@
 module HSH.CommandLineParse where
 
-import Control.Monad.State
-
-import Data.Maybe
-import Data.List
-import Data.List.Split
 import Data.List.Utils
-
 import HSH.ShellState
+
+import Debug.Trace
 
 data ShellAST =
   SetEnv String String
@@ -27,7 +23,7 @@ operationFromString "cd" [directory] = Chdir directory
 operationFromString command args = ExternalCommand command args
 
 expandVariables :: String -> ShellState -> Maybe String
-expandVariables str = maybeExpandVariableReferences (Just str)
+expandVariables = maybeExpandVariableReferences . Just
 
 expand :: [String] -> ShellState -> Maybe [String]
 expand tokens shellstate = mapM expander tokens
@@ -43,32 +39,22 @@ parseLine commandLine shellstate = do
 -- Shell variable substitution code. This code is an absolute hack.
 -}
 
-data SearchMode = Continue | Halt
-
+{- Begin findNextVariableReference -}
 -- Search for the first innermost variable reference and return it.
-findNextVariableReference :: String -> (Char, Int, SearchMode, Maybe EnvVarReference) -> Maybe EnvVarReference
+findNextVariableReference :: String -> (Char, String) -> Maybe String
 
 -- Base cases: Either we run out of String to search, or we hit the first '}' in which case we
 -- immediately return the EnvVarReference.
-findNextVariableReference [] ('}', _, _, varRef) = varRef
-findNextVariableReference [] (_, _, _, _) = Nothing
-findNextVariableReference _ (_, _, Halt, varRef) = varRef
+findNextVariableReference [] (_, _) = Nothing
+findNextVariableReference ('}' : _) (_, reference) = Just $ "${" ++ reverse reference ++ "}"
 
--- We've just seen '${' so make a note of it.
-findNextVariableReference ('{' : rest ) ('$', currentOffset, _, _) =
-  findNextVariableReference rest ('{', currentOffset + 1, Continue, Just (EnvVarReference currentOffset currentOffset))
-
--- We've seen our first '}', which means that we've reached the end of our first-innermost
--- variable reference.
-findNextVariableReference ('}' : rest) (_, currentOffset, Continue, currentVarRef) =
-  findNextVariableReference rest ('}', currentOffset + 1, Halt, Just (fromJust currentVarRef) { end = currentOffset + 1 })
-
+-- We've just seen '${' so start a new variable reference
+findNextVariableReference ('{' : rest ) ('$', _) =
+  findNextVariableReference rest ('{', "")
 -- Keep scanning through the string.
-findNextVariableReference (char : rest) (previousChar, currentOffset, keepConsuming, currentVarRef) =
-  findNextVariableReference rest (char, currentOffset + 1, Continue, currentVarRef)
-
-variableName :: String -> EnvVarReference -> String
-variableName str (EnvVarReference start end) = take (end - start + 1) $ drop (start - 1) str
+findNextVariableReference (char : rest) (x, reference) =
+  findNextVariableReference rest (char, char : reference)
+{- End findNextVariableReference -}
 
 expandVariable :: String -> ShellState -> Maybe String
 expandVariable ('$': '{' : str) shellstate =
@@ -77,15 +63,16 @@ expandVariable ('$': '{' : str) shellstate =
   else Nothing
 expandVariable str _ = Just str
 
-maybeExpandVariableReferences :: Maybe String -> ShellState -> Maybe String
+maybeExpandVariableReferences ::  Maybe String -> ShellState -> Maybe String
 maybeExpandVariableReferences Nothing _ = Nothing
 maybeExpandVariableReferences (Just str) shellstate =
   case innermostVarReference of
-    Just ref -> let varname = variableName str ref in
-                let expansion = expandVariable varname shellstate in
-                  case expansion of
-                    Just exp -> maybeExpandVariableReferences (Just (replace varname exp str)) shellstate
-                    Nothing -> Nothing
+    Just varname -> let expansion = expandVariable varname shellstate in
+                    case expansion of
+                      Just expn ->
+                        let expandedString = replace varname expn str
+                        in maybeExpandVariableReferences (Just expandedString) shellstate
+                      Nothing -> Nothing
     Nothing -> Just str
   where
-    innermostVarReference = findNextVariableReference str ('\0', 0, Continue, Nothing)
+    innermostVarReference = findNextVariableReference str ('\0', "")
